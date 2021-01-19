ea163d795dbcb090f4638f2f44f571b807d79184
Ethan Cope <ethancope47@gmail.com>
2021-01-19 12:20:01 -0600

    modifications to rpi buswarn

--- pybuswarn/oldbuswarn.py
+++ pybuswarn/oldbuswarn.py
@@ -8,213 +8,163 @@ from sevenseg import SevenSeg
 import subprocess as sp
 
 class Route():
-	def __init__(self, userstop="", timedata = [], mode = "normal" ):
-		self.userstop = userstop 
-		self.timedata = timedata
-		self.date = ""
-		self.timeindex = 0
-		self.mode = mode 
-		self.sev = SevenSeg()
-		self.location = "/home/pi/buswarn"
-		self.logfile = open(self.location+"/log","a")
+    def __init__(self, userstop="", timedata = [], mode = "normal" ):
+        self.userstop = userstop 
+        self.timedata = timedata
+        self.date = ""
+        self.timeindex = 0
+        self.mode = mode 
+        self.sev = SevenSeg()
+        self.location = "/home/pi/buswarn"
+        self.logfile = open(self.location+"/log","a")
 
-	def isdebug(self):
-		return self.mode == "debug"
+    def isdebug(self):
+        return self.mode == "debug"
 
-	def downloadSchedule(self, tries = 0):
-		tries +=1 
-		try:
-		 	with open(self.location+"/routes.html","r") as routefile:
-				page = routefile.read()
-		
-				if(t.localtime().tm_mday != self.timedata[1][0].tm_mday):
-					#download schedule
-					raise FileNotFoundError("The schedule is outdated. did the cron job work correctly?")
+    def downloadSchedule(self, tries = 0):
+        tries +=1 
+        try:
+            with open(self.location+"/routes.html","r") as routefile:
+                page = routefile.read()
+        
+                if(t.localtime().tm_mday != self.timedata[1][0].tm_mday):
+                    #download schedule
+                    raise FileNotFoundError("The schedule is outdated. did the cron job work correctly?")
 
-				current_time = t.mktime(t.localtime()) 
+                current_time = t.mktime(t.localtime()) 
 
-				#this handles if the program is started in the middle of the day for whatever reason
-				if self.timeindex == 0:
-					self.timeindex = 1
-					#print("The thing is %s" % len(self.timedata))
-					while t.mktime(self.timedata[self.timeindex][col]) < current_time and self.timeindex < len(self.timedata):
-						#print("Index of %s, column of %s" % (self.timeindex, col))
-						self.timeindex += 1
-						if(self.timeindex == len(self.timedata)):
-							raise FileNotFoundError("The day is over, or your schedule is outdated!")
-					next_time = t.mktime(self.timedata[self.timeindex][col])
-		
-				#elif self.timeindex+1 == len(self.timedata):
-					#raise ValueError("End of bus routes today. Bye!")			self.loadtable(page)
+                #this handles if the program is started in the middle of the day for whatever reason
+                if self.timeindex == 0:
+                    self.timeindex = 1
+                    #print("The thing is %s" % len(self.timedata))
+                    while t.mktime(self.timedata[self.timeindex][col]) < current_time and self.timeindex < len(self.timedata):
+                        #print("Index of %s, column of %s" % (self.timeindex, col))
+                        self.timeindex += 1
+                        if(self.timeindex == len(self.timedata)):
+                            raise FileNotFoundError("The day is over, or your schedule is outdated!")
+                    next_time = t.mktime(self.timedata[self.timeindex][col])
+        
+                #elif self.timeindex+1 == len(self.timedata):
+                    #raise ValueError("End of bus routes today. Bye!")          self.loadtable(page)
 
-		except FileNotFoundError:
-			#wget https://transport.tamu.edu/busroutes/Routes.aspx?r=15 -O /home/pi/buswarn/routes.htmpisnl
+        except FileNotFoundError:
+            #wget https://transport.tamu.edu/busroutes/Routes.aspx?r=15 -O /home/pi/buswarn/routes.htmpisnl
 
-			command = "wget https://transport.tamu.edu/busroutes/Routes.aspx?r=%s -O %s/routes.html"%(routenum, self.location)
-			output = sp.check_output(command.split())
-			self.logfile.write(str(output))
-			print(output)
+            command = "wget https://transport.tamu.edu/busroutes/Routes.aspx?r=%s -O %s/routes.html"%(routenum, self.location)
+            output = sp.check_output(command.split())
+            self.logfile.write(str(output))
+            print(output)
 
-			if tries < 3:
-				return downloadSchedule(tries)
-			else:
-				raise FileNotFoundError("Wget isn't working. check the connection")
+            if tries < 3:
+                return downloadSchedule(tries)
+            else:
+                raise FileNotFoundError("Wget isn't working. check the connection")
 
-	def loadTable(self, page):
-		#page = self.downloadSchedule(refresh = True)
+    def loadTable(self, page):
+        #page = self.downloadSchedule(refresh = True)
 
-		soup = BeautifulSoup(page, "html.parser")
-		table_rows = soup.find('table').find_all('tr')
-		data = []
+        soup = BeautifulSoup(page, "html.parser")
+        table_rows = soup.find('table').find_all('tr')
+        data = []
 
-		#gets header
+        #gets header
 
-		for row in table_rows:
-			td = row.find_all('td')
-			for item in td:
-				if(item.text == "No Service Is Scheduled For This Date"):
-					raise ValueError("No service for buses today: is it a game day?")
+        for row in table_rows:
+            td = row.find_all('td')
+            for item in td:
+                if(item.text == "No Service Is Scheduled For This Date"):
+                    raise ValueError("No service for buses today: is it a game day?")
 
-		for i in range(1,3):
-			th = table_rows[i].find_all('th')
-			row = [i.text for i in th]
-			if row != []:
-				data.append(row)
+        for i in range(1,3):
+            th = table_rows[i].find_all('th')
+            row = [i.text for i in th]
+            if row != []:
+                data.append(row)
 
-		date = str([i.time for i in table_rows[3].find_all('td')]) 
-		self.date = date[date.find('\"')+1:date.find(' ',date.find('\"')+1)] #hardcoded string offset to search behind
-	   
-		#dumb error messages 
-		errormessages = ["Uh oh!", "Stinky!", "Poopies!", "Haha!"]
+        date = str([i.time for i in table_rows[3].find_all('td')]) 
+        self.date = date[date.find('\"')+1:date.find(' ',date.find('\"')+1)] #hardcoded string offset to search behind
+       
+        #dumb error messages 
+        errormessages = ["Uh oh!", "Stinky!", "Poopies!", "Haha!"]
 
-		for tr in table_rows:
-			td = tr.find_all('td')
-			 
-			#print(td)
-			#row = [i.text for i in td]
-			try:
-				row = [t.strptime(self.date+" "+i.text+"M","%m/%d/%Y %I:%M%p") for i in td]
-			except ValueError:
-				if self.mode == "dumb":
-					print(errormessages[int(random.randrange(0,4))])
-				else:
-					print("Some unimportant error")
+        for tr in table_rows:
+            td = tr.find_all('td')
+             
+            #print(td)
+            #row = [i.text for i in td]
+            try:
+                row = [t.strptime(self.date+" "+i.text+"M","%m/%d/%Y %I:%M%p") for i in td]
+            except ValueError:
+                if self.mode == "dumb":
+                    print(errormessages[int(random.randrange(0,4))])
+                else:
+                    print("Some unimportant error")
 
-			if row != []:
-				data.append(row)
+            if row != []:
+                data.append(row)
 
-		self.timedata = data
+        self.timedata = data
 
-	def __str__(self):
-		retstring = ""
-		for line in self.timedata:
-			for tim in line:
-				if type(tim) == type(""):
-					retstring += tim + " "
-				else:
-					retstring += "[%s]"%t.strftime("%I:%M %p", tim)
-			retstring += "\n"
-		return retstring
-			
-	def findNextStop(self):
-		col = self.timedata[0].index(self.userstop)
-		next_time = -1
-		prev_time = -1
-		current_time = t.mktime(t.localtime()) 
-		#to save on resources, we'll have the os start and stop buswarn when it needs	
-		while(True):
-			#secs in day = 86400
-"""
-			#This should be handled on the 
-			if(t.localtime().tm_mday != self.timedata[1][0].tm_mday):
-				#download schedule
-				raise ValueError("The schedule is outdated. did the cron job work correctly?")
+    def __str__(self):
+        retstring = ""
+        for line in self.timedata:
+            for tim in line:
+                if type(tim) == type(""):
+                    retstring += tim + " "
+                else:
+                    retstring += "[%s]"%t.strftime("%I:%M %p", tim)
+            retstring += "\n"
+        return retstring
+            
+    def findNextStop(self):
+        col = self.timedata[0].index(self.userstop)
+        next_time = -1
+        prev_time = -1
+        current_time = t.mktime(t.localtime()) 
+        #to save on resources, we'll have the os start and stop buswarn when it needs   
+        while(True):
+            #secs in day = 86400
 
-			current_time = t.mktime(t.localtime()) 
+            #if the bus leaves
+            if current_time > next_time:
+                self.timeindex+=1
+                next_time = t.mktime(self.timedata[self.timeindex][col])
+                print("Bus left! next bus leaves at [%s]"%t.strftime("%I:%M %p",self.timedata[self.timeindex][col]))
+                self.logfile.write("[%s] %s\n" %(t.strftime("%I:%M %p",t.localtime()),"Bus Left"))
+                self.sev.display([0,0])
+                t.sleep(.5)
+                self.sev.display([-1,-1])
+                t.sleep(.5)
+                self.sev.display([0,0])
+                t.sleep(.5)
+                self.sev.display([-1,-1])
 
-			#this handles if the program is started in the middle of the day for whatever reason
-			if self.timeindex == 0:
-				self.timeindex = 1
-				#print("The thing is %s" % len(self.timedata))
-				while t.mktime(self.timedata[self.timeindex][col]) < current_time and self.timeindex < len(self.timedata):
-					#print("Index of %s, column of %s" % (self.timeindex, col))
-					self.timeindex += 1
-					if(self.timeindex == len(self.timedata)):
-						raise ValueError("The day is over, or your schedule is outdated!")
-				next_time = t.mktime(self.timedata[self.timeindex][col])
-	
-			#elif self.timeindex+1 == len(self.timedata):
-				#raise ValueError("End of bus routes today. Bye!")
-"""
-			#if the bus leaves
-			if current_time > next_time:
-				self.timeindex+=1
-				next_time = t.mktime(self.timedata[self.timeindex][col])
-				print("Bus left! next bus leaves at [%s]"%t.strftime("%I:%M %p",self.timedata[self.timeindex][col]))
-				self.logfile.write("[%s] %s\n" %(t.strftime("%I:%M %p",t.localtime()),"Bus Left"))
-				self.sev.display([0,0])
-				t.sleep(.5)
-				self.sev.display([-1,-1])
-				t.sleep(.5)
-				self.sev.display([0,0])
-				t.sleep(.5)
-				self.sev.display([-1,-1])
+            #TODO - delete the routes file if it's the end of the day!
 
-			#TODO - delete the routes file if it's the end of the day!
+            #handles the minute changing
+            if (next_time - prev_time)//60 != (next_time - current_time)//60:
+                #essentially, if the seconds aren't the same as the last time through the loop
+                diff = (next_time - current_time)/60+1
+                print("%i" % diff)
+                #self.logfile.write("[%s] %i\n" %(t.strftime("%I:%M %p",t.localtime()), diff))
+                self.sev.display([diff//10,diff%10])
+                #+1 is the nice way of rounding so the minutes roughly match up.
+                #also gives lazy people an extra incentive to go earlier I guess
 
-			#handles the minute changing
-			if (next_time - prev_time)//60 != (next_time - current_time)//60:
-				#essentially, if the seconds aren't the same as the last time through the loop
-				diff = (next_time - current_time)/60+1
-				print("%i" % diff)
-				#self.logfile.write("[%s] %i\n" %(t.strftime("%I:%M %p",t.localtime()), diff))
-				self.sev.display([diff//10,diff%10])
-				#+1 is the nice way of rounding so the minutes roughly match up.
-				#also gives lazy people an extra incentive to go earlier I guess
-
-			#print((next_time - current_time)/60)
-			#print("[%s]"%t.strftime("%I:%M %p",self.timedata[self.timeindex][col]))
-			prev_time = current_time
-			t.sleep(5)
-		
-	"""
-	def downloadSchedule(self,routenum = 15, refresh = False):
-		#if schedule isn't already downloaded, downloads it. 
-		tries = 0
-		while(tries < 30):
-			try:
-				tries +=1
-				if refresh:
-					refresh = False
-					raise FileNotFoundError("Haha download now fool")
-				with open(self.location+"/routes.html", 'r') as routefile:
-					page = routefile.read()
-					#print(page)
-					break
-
-			except FileNotFoundError:
-				#wget https://transport.tamu.edu/busroutes/Routes.aspx?r=15 -O /home/pi/buswarn/routes.htmpisnl
-
-				command = "wget https://transport.tamu.edu/busroutes/Routes.aspx?r=%s -O %s/routes.html"%(routenum, self.location)
-				output = sp.check_output(command.split())
-				self.logfile.write(str(output))
-				print(output)
-
-				if tries > 10:
-					print("couldn't wget after 10 tries.")
-					exit(1)
-		return page
-	"""
+            #print((next_time - current_time)/60)
+            #print("[%s]"%t.strftime("%I:%M %p",self.timedata[self.timeindex][col]))
+            prev_time = current_time
+            t.sleep(1)
 
 r = Route("Aggie Station", mode = "debug")
 try:
-	#r.loadTable()
-	r.downloadSchedule()
-	r.findNextStop()
+    #r.loadTable()
+    r.downloadSchedule()
+    r.findNextStop()
 except ValueError as e:
-	r.logfile.write("[%s] %s\n" %(t.strftime("%I:%M %p",t.localtime()), e))
-	print(e)
+    r.logfile.write("[%s] %s\n" %(t.strftime("%I:%M %p",t.localtime()), e))
+    print(e)
 finally:
-	GPIO.cleanup()
+    GPIO.cleanup()
 #print(r)
 
